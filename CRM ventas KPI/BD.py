723c984e11e1ebd8660b083c002c751ac181f1a3
mespases <mespases@nigul.cide.es>
2020-02-13 12:46:55 +0100

    new

--- CRM ventas KPI/BD.py
+++ CRM ventas KPI/BD.py
@@ -1,4 +1,40 @@
+import sqlite3
+
 class BD:
 
-    def select(self):
-        pass
+    # Conexion sql
+    sql = sqlite3.connect("kpi.db", timeout=10)
+    cursor = sql.cursor()
+
+    productos = []
+
+    def selectProductos(self):
+        """ Muestra por pantalla la lista de los productos """
+        consulta = "SELECT Nombre FROM Producto;"
+        result = self.sql.execute(consulta)
+
+        print("\nLista de productos: ")
+        for producto in result:
+            print(producto[0])
+            self.productos.append(producto[0])
+
+    def crearFacturaBD(self, producto, cantidad, nombre):
+        consulta = "SELECT Precio FROM Producto WHERE Nombre = '{}';".format(producto)
+        result = self.sql.execute(consulta)
+
+        for i in result:
+            precio = i[0]
+
+        total = precio*cantidad
+        insert = "INSERT INTO Factura (Nombre_cliente, Total, Num_articulos_comprados) VALUES ('{}', {}, {});".format(nombre, total, cantidad)
+        self.sql.execute(insert)
+        self.sql.commit()
+
+        return total
+
+    def sumarCantidades(self, producto, cantidad):
+        consulta = "UPDATE Producto SET Cantidad_vendida_diaria = Cantidad_vendida_diaria+{0}, Cantidad_vendida_mensual = Cantidad_vendida_mensual+{0}, " \
+                   "Cantidad_vendida_trimestral = Cantidad_vendida_trimestral+{0}, Cantidad_vendida_anual = Cantidad_vendida_anual+{0} WHERE Nombre = '{1}';".format(cantidad, producto)
+        self.sql.execute(consulta)
+        self.sql.commit()
+
--- CRM ventas KPI/CRM.py
+++ CRM ventas KPI/CRM.py
@@ -11,7 +11,7 @@ class CRM:
             salir del menu, este imprime el menu y permite
             seleccionar la opcion """
         opcion = 0
-        while opcion != 8:
+        while opcion != 7:
             self.__printOpciones()
             opcion = int(input("\nIntroduce una opcion: "))
             self.__elegirOpcionesMenu(opcion)
@@ -21,13 +21,12 @@ class CRM:
             en el menu """
         print("\n================Menu================")
         print("1) Vender")
-        print("2) Crear Factura")
-        print("3) Precio por producto")
-        print("4) Ver media de articulos por venta")
-        print("5) Media ingresos de tienda")
-        print("6) Top 5 productos mas vendidos")
-        print("7) Top 5 productos menos vendidos")
-        print("8) Salir del menu")
+        print("2) Precio por producto")
+        print("3) Ver media de articulos por venta")
+        print("4) Media ingresos de tienda")
+        print("5) Top 5 productos mas vendidos")
+        print("6) Top 5 productos menos vendidos")
+        print("7) Salir del menu")
 
     def __printOpcionesSecundarias(self, mediasDe):
         """ Imprime las opciones secundarias disponibles en el
@@ -49,8 +48,7 @@ class CRM:
         """ Selecciona la opcion elegida en el menu y posteriormente
             hara el metodo correspondiente para cada opcion elegida """
         if opcion == 1:
-            # Falta pedir y comprobar el producto a vender, junto a las cantidades
-            self.banco.realizarPago()
+            self.__vender()
         elif opcion == 2:
             pass
         elif opcion == 3:
@@ -58,15 +56,14 @@ class CRM:
         elif opcion == 4:
             pass
         elif opcion == 5:
-            pass
-        elif opcion == 6:
             self.__printOpcionesSecundarias("articulos")
             opcionSecundaria = int(input("\nIntroduce una opcion: "))
-        elif opcion == 7:
+        elif opcion == 6:
             self.__printOpcionesSecundarias("tienda")
             opcionSecundaria = int(input("\nIntroduce una opcion: "))
-        elif opcion == 8:
+        elif opcion == 7:
             print("Saliendo del menu")
+            self.bd.sql.close()
         else:
             print("La opcion insertada es incorrecta")
 
@@ -93,6 +90,31 @@ class CRM:
             elif opcionesSecundarias == 4:
                 pass
 
+    def __vender(self):
+        """ Vende el producto seleccionado y pide al cliente si
+            quiere factura o no """
+        self.bd.selectProductos()
+        producto = raw_input("\nQue producto vas a comprar: ")
+        if producto.title() in self.bd.productos:
+            cantidad = int(raw_input("Introduce la cantidad deseada: "))
+            nombre = raw_input("\nNombre del cliente: ")
+
+            factura = raw_input("Quiere factura, S/N: ")
+            if factura.upper() == "S":
+                self.__crearFactura(cantidad, nombre, producto.title())
+                self.bd.sumarCantidades(producto, cantidad)
+                self.banco.realizarPago()
+        else:
+            print("El producto insertado no es correcto")
+
+    def __crearFactura(self, cantidad, nombre, producto):
+        """ Crea la factura del cliente """
+        total = self.bd.crearFacturaBD(producto, cantidad, nombre)
+        print("\nFACTURA")
+        print("El nombre del cliente es: {}").format(nombre)
+        print("El producto comprado del cliente es: {}").format(producto)
+        print("La cantidad del producto del cliente es: {}").format(cantidad)
+        print("El total de la factura es: {}\n").format(total)
 
 a = CRM()
 a.menu()
